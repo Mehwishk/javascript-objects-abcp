7adf0f2b8878ab6db4a79e5df321d4b07aa05eca
Mehwish Kapadia <mehwishkap@gmail.com>
2018-01-18 20:02:40 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,4 +3,7 @@ var playlist = {
   ArRahman: 'Jai Ho', 
   ArjitSingh: 'Sun zara'}
   
+function updatePlaylist (targetObject,updatesObject){
+  return Object.assign({},targetObject,updatesObject)
+}
 
